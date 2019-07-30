a560430cd5ec2faa8d9a7a531eaa47b77ddd97d2
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-07-31 00:44:33 +0100

    Create Blockchain_implementation.py

---
+++ Show_Me_The_Data_Structures_Project_Nanodegree/Blockchain_implementation.py
@@ -0,0 +1,4 @@
+"""
+A Blockchain is a sequential chain of records, similar to a linked list. Each block contains some information and how it is connected related to the other blocks in the chain. Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data. For our blockchain we will be using a SHA-256 hash, the Greenwich Mean Time when the block was created, and text strings as the data.
+Use your knowledge of linked lists and hashing to create a blockchain implementation.
+"""
