67e4fba13bae540c34aa896fc76796cc52ae2496
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-08-04 03:57:43 +0100

    Update Task4.py

--- Unscramble_Computer_Science_Problems/Task4.py
+++ Unscramble_Computer_Science_Problems/Task4.py
@@ -1,1 +1,26 @@
 
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+ongoingCalls, callsReceivers, ongoingTexts, textsReceivers, telemaketersNumbers = ([] for i in range(5))
+
+for line in calls:
+    ongoingCalls.append(line[0])
+    callsReceivers.append(line[1])
+
+for line in texts:
+    ongoingTexts.append(line[0])
+    textsReceivers.append(line[1])
+
+telemaketersNumbers = list(set(ongoingCalls) - set(textsReceivers) - set(callsReceivers) - set(ongoingTexts))
